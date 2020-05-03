7a43369aa426bade90207ab4aadbf3000c7e8567
jeovane <jeovanearaujo16@gmail.com>
2020-05-03 16:57:51 -0300

    Criação do componente Gift

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <h1>Problema Monty Hall</h1>
+    <Gift/>
+  </div>
+</template>
+
+<script>
+import Gift from "./components/Gift";
+
+export default {
+  name: 'App',
+  components: {
+    Gift
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    color: #FFF;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+  }
+
+  .form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+  }
+
+  .doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+</style>
