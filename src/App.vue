558024f6ab0e346c8031d9b4294ecbaeacce3b72
jeovane <jeovanearaujo16@gmail.com>
2020-05-03 17:22:12 -0300

    Criação do componente Door

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <h1>Problema Monty Hall</h1>
-    <Gift/>
+    <Door number="1" :hasGift="false" />
   </div>
 </template>
 
 <script>
-import Gift from "./components/Gift";
+import Door from "./components/Door";
 
 export default {
   name: 'App',
   components: {
-    Gift
+    Door
   }
 }
 </script>
